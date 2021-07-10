ac5ad544d30b7c976623aba01db177b0c546ff21
MAGANER <maganer.krik@mail.ru>
2021-07-10 13:13:59 +0300

    added tab printing

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -3,10 +3,52 @@ using namespace TabManager;
 
 MainMenu::MainMenu()
 {
-	for (int i = 0; i < string_number; i++)
-		tab_field[string_names[i]] = vector<char>();
+	init_field();
+
+	run();
 }
 MainMenu::~MainMenu()
 {
+	
+}
+void MainMenu::init_field()
+{
+	for (size_t i = 0; i < string_number; i++)
+		tab_field[string_names[i]] = vector<char>();
+}
+void MainMenu::print_field()
+{
+	//print in order of insertion
+	for (size_t i = 0; i < string_number; i++)
+	{
+		auto curr = string_names[i];
+		auto tab = tab_field[curr];
+		cout << curr << "|";
+		for (auto& point : tab) cout << point<<" ";
+		cout << endl;
+	}
+}
+void MainMenu::increase_field()
+{
+	for_each(tab_field.begin(), tab_field.end(),
+		[&](pair<char,cvec> val)
+		{
+			val.second.push_back('-');
+		});
+}
 
+void MainMenu::run()
+{
+	while (true)
+	{
+		if (any_action)
+		{
+			print_field();
+			any_action = false;
+		}
+		if (_kbhit())
+		{
+
+		}
+	}
 }
--- MainMenu.h
+++ MainMenu.h
@@ -1,20 +1,32 @@
 #pragma once
 #include<vector>
 #include<map>
+#include<algorithm>
 #include<iostream>
+#include<conio.h>
+#include<stdio.h>
 namespace TabManager
 {
 using namespace std;
 
+typedef vector<char> cvec;
 class MainMenu
 {
 private:
-	map<char, vector<char>> tab_field; //for 6-string guitar
+	map<char, cvec> tab_field; //for 6-string guitar
 	const char* string_names   = "eBGDAE\0";
 	const size_t string_number = 6;
+
+	bool any_action = true;
 public:
 	MainMenu();
 	~MainMenu();
+private:
+	void init_field();
+
+	void print_field();
+	void increase_field();
+	void run();
 };
 };
 
