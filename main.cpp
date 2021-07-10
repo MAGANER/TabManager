894855635a13bde9b029c15e723f632e54ac7617
MAGANER <maganer.krik@mail.ru>
2021-07-10 13:51:43 +0300

    hide cursor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include"MainMenu.h"
+
+void hidecursor()
+{
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;
+	info.bVisible = FALSE;
+	SetConsoleCursorInfo(consoleHandle, &info);
+}
 int main()
 {
+	system("cls");
+	hidecursor();
+
 	namespace tm = TabManager;
 	tm::MainMenu app;
 
