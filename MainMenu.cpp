bfd8f8d9f0ded8431b48364fc06f3164a5a3c8f3
MAGANER <maganer.krik@mail.ru>
2021-07-10 15:05:04 +0300

    made ability to move around x and y, add values to tab cell and save it to text file

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -14,7 +14,7 @@ MainMenu::~MainMenu()
 void MainMenu::init_field()
 {
 	for (size_t i = 0; i < string_number; i++)
-		tab_field[string_names[i]] = vector<char>();
+		tab_field[string_names[i]] = svec();
 }
 void MainMenu::print_field()
 {
@@ -23,14 +23,28 @@ void MainMenu::print_field()
 	{
 		auto curr = string_names[i];
 		auto tab = tab_field[curr];
-		cout << curr << "|";
-		for (auto& point : tab) cout << point<<" ";
+		if (current_pos.y == i) setcolor(COLORS::RED, COLORS::BLACK);
+		else setcolor(COLORS::GREEN, COLORS::BLACK);
+
+		cout << curr;
+		setcolor(COLORS::GREEN, COLORS::BLACK);
+		cout << "|";
+		for (size_t n= 0;n<tab.size();n++)
+		{
+			auto point = tab[n];
+			if (current_pos.x == n &&
+				current_pos.y == i) 
+				setcolor(COLORS::MAGENTA, COLORS::BLACK);
+			else setcolor(COLORS::GREEN, COLORS::BLACK);
+			cout << point << " ";
+			setcolor(COLORS::GREEN, COLORS::BLACK);
+		}
 		cout << endl;
 	}
 }
 void MainMenu::increase_field()
 {
-	for (auto& field : tab_field) field.second.push_back('-');
+	for (auto& field : tab_field) field.second.push_back("-");
 }
 
 void MainMenu::run()
@@ -42,15 +56,104 @@ void MainMenu::run()
 			print_field();
 			any_action = false;
 		}
-		if (_kbhit())
+		if (_kbhit() && !key_pressed)
 		{
 			int input = _getch();
 			switch ((CODES)input)
 			{
 			case CODES::i: increase_field(); break;
+			case CODES::a: add_new_value();  break;
+			case CODES::s: save();           break;
+
+			case CODES::h: move(LEFT);  break;
+			case CODES::l: move(RIGHT); break;
+			case CODES::j: move(DOWN);  break;
+			case CODES::k: move(UP);    break;
 			};
 			any_action = true;
 			system("cls");
+			key_pressed = true;
+		}
+		if (!_kbhit())key_pressed = false;
+	}
+}
+
+bool MainMenu::can_move(const Coord& increaser)
+{
+	Coord new_pos = { current_pos.x + increaser.x,
+					 current_pos.y + increaser.y };
+	if (new_pos.x < 0 || new_pos.y < 0) return 0;
+
+	Coord max = get_max_pos();
+
+	if (increaser.x == 0) return new_pos.y < max.y;
+	if (increaser.y == 0) return new_pos.x < max.x;
+}
+Coord MainMenu::get_max_pos()
+{
+	return Coord{ (int)tab_field['e'].size(), (int)string_number};
+}
+void MainMenu::move(const Coord& dir)
+{
+	if (can_move(dir))
+	{
+		current_pos.x += dir.x;
+		current_pos.y += dir.y;
+	}
+}
+void MainMenu::setcolor(COLORS _textcol, COLORS _backcol)
+{
+	int textcol = (int)_textcol;
+	int backcol = (int)_backcol;
+	if ((textcol % 16) == (backcol % 16))textcol++;
+	textcol %= 16; backcol %= 16;
+	unsigned short wAttributes = ((unsigned)backcol << 4) | (unsigned)textcol;
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hStdOut, wAttributes);
+}
+void MainMenu::add_new_value()
+{
+	string val;
+	cout << "value:";
+	cin >> val;
+	insert_to_field(val);
+}
+void MainMenu::insert_to_field(const string& val)
+{
+	for (size_t i = 0; i < string_number; i++)
+	{
+		if (i == current_pos.y)
+		{
+			auto name = string_names[i];
+			tab_field[name][current_pos.x] = val;
 		}
 	}
+}
+void MainMenu::write_tab_down(const string& path)
+{
+	ofstream file(path);
+
+	vector<string> values;
+	for (size_t i = 0; i < string_number; i++)
+	{
+		string val;
+		auto name = string_names[i];
+		val += name;
+		val += "|";
+		for (auto v : tab_field[name]) val += v;
+		values.push_back(val);
+	}
+	for (auto v : values)
+	{
+		file << v << endl;
+	}
+
+	file.close();
+}
+void MainMenu::save()
+{
+	string path;
+	cout << "path:";
+	cin >> path;
+	write_tab_down(path);
 }
--- MainMenu.h
+++ MainMenu.h
@@ -1,18 +1,28 @@
 #pragma once
+#include<Windows.h>
 #include<vector>
 #include<map>
 #include<iostream>
+#include<fstream>
+#include<string>
 #include<conio.h>
 #include<stdio.h>
+
 namespace TabManager
 {
 using namespace std;
 
-typedef vector<char> cvec;
+
+struct Coord
+{
+	int x, y;
+};
+
+typedef vector<string> svec;
 class MainMenu
 {
 private:
-	map<char, cvec> tab_field; //for 6-string guitar
+	map<char, svec> tab_field; //for 6-string guitar
 	const char* string_names   = "eBGDAE\0";
 	const size_t string_number = 6;
 
@@ -20,8 +30,33 @@ private:
 
 	enum class CODES
 	{
-		i = 105,
+		i = 105, //increase field
+		a = 97,  //add new value
+		s = 115, //save
+
+		j = 106, //down
+		k = 107, //up
+		h = 104, //left
+		l = 108, //right
 	};
+	enum class COLORS
+	{
+		BLACK   = 0,
+		GREEN   = 2,
+		RED     = 4,
+		MAGENTA = 5,
+	};
+
+	Coord start_position = { 2,0 };
+	Coord current_pos = { 0, 0 }; //y can not be higher then 6
+
+	//moving directions
+	const Coord LEFT { -1,0  };
+	const Coord RIGHT{ 1,0   };
+	const Coord UP   { 0,-1  };
+	const Coord DOWN { 0,1   };
+
+	bool key_pressed = false;
 public:
 	MainMenu();
 	~MainMenu();
@@ -30,7 +65,20 @@ private:
 
 	void print_field();
 	inline void increase_field();
+
 	void run();
+
+	void move(const Coord& dir);
+	bool can_move(const Coord& increaser);
+	Coord get_max_pos();
+
+	void setcolor(COLORS _textcol, COLORS backcol);
+
+	void add_new_value();
+	void insert_to_field(const string& val);
+
+	void save();
+	void write_tab_down(const string& path);
 };
 };
 
